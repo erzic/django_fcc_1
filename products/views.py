1d62db5b2490f6d528045c36785453f41eaf37d5
unknown <erickbarrios3@gmail.com>
2020-08-06 21:10:52 -0600

    just before leaving

--- products/views.py
+++ products/views.py
@@ -1,7 +1,49 @@
 from django.shortcuts import render
-
+from .forms import ProductForm, RawProductForm
 from .models import Product
+
+
+
 # Create your views here.
+
+def product_create_view(request):
+	my_form = RawProductForm()
+	if request.method == "POST":
+		form = RawProductForm(request.POST)
+		if my_form.is_valid():
+			# now the data is good
+			print(my_form.cleaned_data)
+			Product.objects.create(**my_form.cleaned_data)
+		else:
+			print(my_form.errors)
+	context = {
+		"form": my_form
+
+	}
+	return render(request=request, template_name = "products/product_create.html", context = context)
+
+
+# def product_create_view(request):
+# 	# print(request.GET)
+# 	# print(request.POST)
+# 	my_new_title = request.POST.get('title')
+# 	print(my_new_title)
+# 	context = {}
+# 	return render(request=request, template_name = "products/product_create.html", context = context)
+
+
+# def product_create_view(request):
+# 	form = ProductForm(request.POST or None)
+# 	if form.is_valid():
+# 		form.save()
+# 		form = ProductForm()
+
+# 	context = {
+# 		'form':form
+# 	}
+
+# 	return render(request, template_name = "products/product_create.html",
+# 		context = context)
 
 def product_detail_view(request):
 	obj = Product.objects.get(id=3)
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view, about_view, contact_view, base_view
-from products.views import product_detail_view
+from products.views import product_detail_view, product_create_view
 
 urlpatterns = [
 	path("", home_view, name="home"),
@@ -25,5 +25,6 @@ urlpatterns = [
     path("contact/", contact_view, name = "contact"),
     path("base/", base_view, name="base"),
     path("product/", product_detail_view),
+    path("create/", product_create_view),
     path('admin/', admin.site.urls),
 ]
